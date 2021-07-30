33206c1397320f57792ddcea79fd6fb5156da9b2
Jessica Moody <jessicamoody1988@gmail.com>
2021-07-30 02:13:01 -0500

    Exercise: Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -13,6 +13,7 @@ import Contact from './ContactComponent'
 import Directory from './DirectoryComponent'
 import CampsiteInfo from './CampsiteInfoComponent';
 import Home from './HomeComponent';
+import Reservation from './ReservationComponent';
 import {
     fetchCampsites,
     fetchComments,
@@ -125,6 +126,29 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation },
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -168,6 +192,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Reservation: { 
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
