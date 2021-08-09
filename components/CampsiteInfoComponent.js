841642b27f3743cbdbb772ffbb2603fa9461abb1
Jessica Moody <jessicamoody1988@gmail.com>
2021-08-08 22:03:25 -0500

    Week 3 Workshop Assignment Task 3 INCOMPLETE

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -37,6 +37,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
+    const recognizeComment = ({ dx }) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -65,6 +66,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                <CampsiteInfo onShowModal={true} />
             }
             return true;
         }
@@ -155,7 +158,7 @@ class CampsiteInfo extends Component {
             rating: 5,
             author: '',
             text: ''
-        }
+        };
     }
 
     toggleModal() {
